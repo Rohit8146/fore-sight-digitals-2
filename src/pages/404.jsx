cb53917e2a6d35067a6697ffe3e6ad78e93105dc
Rohit8146 <rohittandon545@gmail.com>
2025-09-22 11:56:00 +0530

    Style Fixes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-black text-white">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <h1 className="text-6xl font-bold mb-4">404!!</h1>
       <p className="text-xl mb-6">Oops! The page you're looking for doesn’t exist.</p>
       <Link
         to="/"
