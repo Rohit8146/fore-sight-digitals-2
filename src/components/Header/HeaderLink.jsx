2d781a9c09e9e3eb54c60962122d56d8db7d8e65
Rohit8146 <rohittandon545@gmail.com>
2025-09-25 21:16:48 +0530

    header footer fixes

--- src/components/Header/HeaderLink.jsx
+++ src/components/Header/HeaderLink.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 function HeaderLink({ item, index }) {
   return (
     <li key={index} className="text-white font-semibold text-lg">
-      <a
+      <a 
         href={item.link}
         className={({ isActive }) =>
           `${isActive ? "active" : ""} underline-hover`
